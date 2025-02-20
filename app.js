1012ac9a36a2394ef1ea2935aae15474e54a8dd9
Erick Trindade <erick.trindade@ORIT.LOCAL>
2025-02-20 18:15:11 -0300

    feat: Adicionado função adicionarAmigo e renderizarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+const btnAdicionarAmigo = document.querySelector(".button-add");
+const inputAmigo = document.querySelector("#amigo");
+const listaAmigos = document.querySelector("#listaAmigos");
+
+let amigos = [];
+
+function adicionarAmigo() {
+  const amigo = inputAmigo.value;
+
+  amigo ? amigos.push(amigo) : alert("Digite o nome do amigo");
+
+  inputAmigo.value = "";
+
+  renderizaAmigo();
+}
+
+function renderizaAmigo() {
+  listaAmigos.innerHTML = "";
+
+  amigos.forEach((amigo) => {
+    const li = document.createElement("li");
+
+    li.textContent = amigo;
+    listaAmigos.appendChild(li);
+  });
+}
